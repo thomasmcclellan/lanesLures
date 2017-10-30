edc6f14cd6b3a382a4039e658b1e3fbe033a1b32
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-10-30 15:29:53 -0400

    Checkout page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
+import { CheckoutComponent } from "./home/checkout/checkout.component";
+import { HomeComponent } from "./home/home.component";
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'home', component: HomeComponent }
 //   { path: 'home', component: HomeComponent, canActivate: [AuthNullGuard || AuthGuard]  },
 ];
 
