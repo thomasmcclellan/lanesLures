2d224cba0b02a856b89453577c0e2695aaa2c07d
Nathan Bogan <bogannathan@gmail.com>
2017-10-30 15:26:14 -0400

    added signup with validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from "./auth/signin/signin.component";
+import { HomeComponent } from "./home/home.component";
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},  
+  { path: 'signin', component: SigninComponent}
 //   { path: 'home', component: HomeComponent, canActivate: [AuthNullGuard || AuthGuard]  },
 ];
 
