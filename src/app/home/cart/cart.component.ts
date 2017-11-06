25a471708d8b157dcea2d50498236491d2cf4748
Nathan Bogan <bogannathan@gmail.com>
2017-11-06 13:29:33 -0500

    update products in cart, show total quantity and total price in cart

--- src/app/home/cart/cart.component.ts
+++ src/app/home/cart/cart.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  cart
+  total
 
   constructor() { }
 
   ngOnInit() {
+    this.cart = JSON.parse(sessionStorage.getItem('cart'))
+    this.total = this.cart.map(item => item.thisItemTotal).reduce((a, b) => a + b, 0)
+    console.log(this.total)
   }
 
 }
